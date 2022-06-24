3f6e4f480545959d7ee9c593e067fca5f45b2cb7
ParkSoyun <soyp@naver.com>
2022-06-25 01:19:47 +0900

    First Try: [프로그래머스] 이상한 문자 만들기
    * 공백 처리에 관한 오류 발생

---
+++ Algorithm05.java
@@ -0,0 +1,44 @@
+/*
+ * 문자열 s는 한 개 이상의 단어로 구성되어 있습니다.
+ * 각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+ * 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+ *
+ *
+ * 문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+ * 첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+ */
+
+public class Algorithm05 {
+    public String solution(String s) {
+        String[] s_split = s.split(" ");
+
+        StringBuilder answer_sb = new StringBuilder();
+        String answer = "";
+
+        for(int i=0; i<s_split.length; i++) {
+            String[] s_split_split = s_split[i].split("");
+
+            for(int j=0; j<s_split[i].length(); j++) {
+                if(j % 2 == 0) {
+                    answer_sb.append(s_split_split[j].toUpperCase());
+                } else {
+                    answer_sb.append(s_split_split[j].toLowerCase());
+                }
+            }
+
+            if(i < s_split.length-1) {
+                answer_sb.append(" ");
+            }
+        }
+
+        answer = answer_sb.toString();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String result = new Algorithm05().solution("try hello world");
+
+        System.out.println("result: " + result);
+    }
+}
